2d26782a094a3c1715156d0ee9ea69dfdf1942ef
Raj Patel <rajpatel131298@gmail.com>
2023-02-20 20:16:35 -0500

    Updated internet page for daily usage

--- public/js/internet.js
+++ public/js/internet.js
@@ -1,4 +1,24 @@
 function updateEquipmentData(){
-     var mac = document.getElementById("identifierList").value;
-     console.log("Mac selected: "+mac);
+    var mac = document.getElementById("identifierList").value;
+    console.log("Mac selected: "+mac);
+    // var identifier_list = "<%= user_data.identifier_list %>";
+    // console.log("Mac selected: "+identifier_list);
+
+    $.ajax({
+        url: '/getUsageData',
+        type: 'POST',
+        data: {
+            identifier: mac
+        },
+        // jsonpCallback: 'callback', // this is not relevant to the POST anymore
+        success: function (result) {
+            console.log(result)
+            document.getElementById("daily_upload").innerHTML = result.user_data.daily_upload;
+            document.getElementById("daily_download").innerHTML = result.user_data.daily_download;
+            document.getElementById("daily_total").innerHTML = result.user_data.daily_total;
+        },
+        error: function (xhr, status, error) {
+            console.log('Error: ' + error.message);
+        },
+    });
  }
